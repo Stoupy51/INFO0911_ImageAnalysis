784e5836983541223e445390c80d9cde6a8672b5
Stoupy51 <stoupy51@gmail.com>
2024-09-16 21:29:15 +0200

    🚧 Basic files structure

---
+++ config.py
@@ -0,0 +1,17 @@
+
+## Configuration file for the application
+import time
+import os
+
+# Constants for start time
+START_TIME: float = time.time()
+START_TIME_STR: str = time.strftime("%Y-%m-%d %H:%M:%S")
+
+# Folders
+ROOT: str = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")	# Root folder (where the config.py file is located)
+IMAGE_FOLDER: str = f"{ROOT}/images"										# Folder where the images are stored
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,23 @@
+
+# Imports
+from config import *
+from src.print import *
+
+# Main function
+def main():
+
+	# Print the start time
+	info(f"Start time: {START_TIME_STR} ({START_TIME})")
+
+
+
+	# End of the script
+	info("End of the script")
+	return
+
+
+
+# Entry point of the script
+if __name__ == "__main__":
+	main()
+
